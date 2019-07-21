510730a80790f6f54ac34cd0fbc19f12a64a8f00
sehannp <sehanp@gmail.com>
2019-07-21 16:46:51 +1000

    updated with the second page and style

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+
 import './menu-item.styles.scss';
 
-//destructured props used
-const MenuItem = ({title, imageUrl, size})=> (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   <div
-    className={`${size} menuitem`}
-    className='menu-item'
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
   >
-
-   <div className="background-image" style = {
-     {backgroundImage: `url(${imageUrl})`}
-   }
-   >
-   </div>
-
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
     <div className='content'>
-      <h1 className='title'>{title}</h1>
+      <h1 className='title'>{title.toUpperCase()}</h1>
       <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
-)
+);
 
-
-export default MenuItem
+export default withRouter(MenuItem);
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+
+import Directory from '../../components/directory/directory.component';
+
 import './homepage.styles.scss';
-import Directory from '../../components/directory/directory.component'
 
-const Homepage = () => (
+const HomePage = () => (
   <div className='homepage'>
-    <Directory/>
+    <Directory />
   </div>
-)
+);
 
-export default Homepage
+export default HomePage;
