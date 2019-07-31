7e48627ea241205debdc2627a708d46bc115562b
sehannp <sehanp@gmail.com>
2019-07-31 16:15:11 +1000

    Added checkoutpage, checkoutitem component and updated cart to use selectors from reselect

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,25 +1,37 @@
 import React from "react";
 import "./cart-dropdown.styles.scss";
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { withRouter} from 'react-router-dom';
+
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selector';
+import { toggleCartHidden } from '../../redux/cart/cart.actions.js';
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className="cart-dropdown" >
     <div className="cart-items">
       {
+        cartItems.length ?
         cartItems.map(cartItem => (
           <CartItem key={cartItem.id} item={cartItem}/>
-     ))}
-      <CustomButton> GO TO CHECKOUT </CustomButton>
+        )) :
+        <span className="empty-message">Your cart is empty</span>
+      }
+      <CustomButton onClick={ () => {
+          history.push('/checkout');
+          dispatch(toggleCartHidden());
+          }
+      }> GO TO CHECKOUT </CustomButton>
     </div>
   </div>
 )
 
 //destructuring
-const mapStateToProps = ({ cart: {cartItems} }) => ({
-  cartItems
+const mapStateToProps = createStructuredSelector ({
+  cartItems:selectCartItems
 });
 
-
-export default connect(mapStateToProps)(CartDropdown);
+//if we dont specify dispatch as argument, it will be still available for use in the head
+export default withRouter(connect(mapStateToProps)(CartDropdown));
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
 import './header.style.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
+import { selectCartHidden } from '../../redux/cart/cart.selector';
+import { selectCurrentUser } from '../../redux/user/user.selector';
 
 const Header = ({ currentUser, hidden }) => (
   <div className='header'>
@@ -32,10 +37,10 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden} }) => ({
-  currentUser,
-  hidden
-})
+const mapStateToProps = (state) => createStructuredSelector({
+  currentUser:selectCurrentUser,
+  hidden:selectCartHidden
+});
 
 /*Basic
 const mapStateToProps = state => ({
